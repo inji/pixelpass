e31508cb22785f0ac7c527edf117e53efdc6b77b
srikanth716 <srikanthsri7447@gmail.com>
2025-12-23 01:47:40 +0530

    [INJICERT-1234] Add case-insensitive mapper support
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- kotlin/PixelPass/src/commonMain/kotlin/io/mosip/pixelpass/utils/MapperUtil.kt
+++ kotlin/PixelPass/src/commonMain/kotlin/io/mosip/pixelpass/utils/MapperUtil.kt
@@ -4,51 +4,78 @@ import org.json.JSONArray
 import org.json.JSONObject
 
 fun JSONObject.toMapWithKeyAndValueMapper(
-    keyMapper: Map<String, Any> = emptyMap(),
-    valueMapper: Map<String, Map<Any, Any>> = emptyMap()
+  keyMapper: Map<String, Any> = emptyMap(),
+  valueMapper: Map<String, Map<Any, Any>> = emptyMap()
 ): Map<Any, Any?> {
-    val map = mutableMapOf<Any, Any?>()
-    val keys = this.keys()
 
-    while (keys.hasNext()) {
-        val originalKey: String = keys.next().toString()
+  val normalizedKeyMapper: Map<String, Any> = keyMapper.mapKeys { it.key.toLowerCase() }
 
-        val newKey: Any = keyMapper[originalKey] ?: originalKey
-
-        val originalValue = this.get(originalKey)
-
-        val fieldValueMapper = valueMapper[originalKey]
-
-        var processedValue: Any? = originalValue
-
-        if (originalValue != JSONObject.NULL && fieldValueMapper != null) {
-            processedValue = fieldValueMapper[originalValue] ?: originalValue
+  val normalizedValueMapper: Map<String, Map<Any, Any>> =
+    valueMapper
+      .mapKeys { it.key.toLowerCase() }
+      .mapValues { (_, mapper) ->
+        mapper.mapKeys { entry ->
+          val key = entry.key
+          if (key is String) key.toLowerCase() else key
         }
+      }
 
-        processedValue = when (processedValue) {
-            is JSONObject -> processedValue.toMapWithKeyAndValueMapper(keyMapper, valueMapper)
-            is JSONArray -> processedValue.toListWithKeyAndValueMapper(keyMapper, valueMapper)
-            JSONObject.NULL -> null
-            else -> processedValue
-        }
+  val map = mutableMapOf<Any, Any?>()
+  val keys = this.keys()
 
-        map[newKey] = processedValue
-    }
-    return map
+  while (keys.hasNext()) {
+    val originalKey = keys.next() as String
+    val normalizedKey = originalKey.toLowerCase()
+
+    val newKey: Any = normalizedKeyMapper[normalizedKey] ?: originalKey
+
+    val originalValue = this.get(originalKey)
+
+    val fieldValueMapper = normalizedValueMapper[normalizedKey]
+
+    var processedValue: Any? =
+      if (originalValue != JSONObject.NULL && fieldValueMapper != null) {
+        val lookupValue =
+          if (originalValue is String) originalValue.toLowerCase() else originalValue
+
+        fieldValueMapper[lookupValue] ?: originalValue
+      } else {
+        originalValue
+      }
+
+    processedValue =
+      when (processedValue) {
+        is JSONObject -> processedValue.toMapWithKeyAndValueMapper(keyMapper, valueMapper)
+
+        is JSONArray -> processedValue.toListWithKeyAndValueMapper(keyMapper, valueMapper)
+
+        JSONObject.NULL -> null
+        else -> processedValue
+      }
+
+    map[newKey] = processedValue
+  }
+
+  return map
 }
+
 fun JSONArray.toListWithKeyAndValueMapper(
-    keyMapper: Map<String, Any> = emptyMap(),
-    valueMapper: Map<String, Map<Any, Any>> = emptyMap()
+  keyMapper: Map<String, Any> = emptyMap(),
+  valueMapper: Map<String, Map<Any, Any>> = emptyMap(),
 ): List<Any?> {
-    val list = mutableListOf<Any?>()
-    for (i in 0 until this.length()) {
-        val processedValue: Any? = when (val value = this.get(i)) {
-            is JSONObject -> value.toMapWithKeyAndValueMapper(keyMapper, valueMapper)
-            is JSONArray -> value.toListWithKeyAndValueMapper(keyMapper, valueMapper)
-            JSONObject.NULL -> null
-            else -> value
-        }
-        list.add(processedValue)
-    }
-    return list
+
+  val list = mutableListOf<Any?>()
+
+  for (i in 0 until this.length()) {
+    val processedValue: Any? =
+      when (val value = this.get(i)) {
+        is JSONObject -> value.toMapWithKeyAndValueMapper(keyMapper, valueMapper)
+        is JSONArray -> value.toListWithKeyAndValueMapper(keyMapper, valueMapper)
+        JSONObject.NULL -> null
+        else -> value
+      }
+    list.add(processedValue)
+  }
+
+  return list
 }
